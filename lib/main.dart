ca9296c1efcb5f4b290ec7d1e3cf43695c307d15
Vineel Mathew <37242738+vineelmathew@users.noreply.github.com>
2020-06-21 23:10:57 +0530

    need to add flash

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:lightbulb/living_home/living_home_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: LivingHomePage(),
+    );
+  }
+}
